c7d088296e9bf23fd9a18cb2fcfb4f01717f56f9
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2022-10-20 14:25:48 +0530

    display queue

--- October/queue.c
+++ October/queue.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
-#define MAX 5
+#define MAX 3
 int R = -1, F = -1, ITEM, QUEUE[MAX];
-void enqueue()
-{
+void enqueue(){
     if (R == MAX - 1)
         printf("\nQueue Full\n");   
-    else if (F==-1 && R==-1)
-    {
+    else if (F==-1 && R==-1){
       F = R = 0;
       printf("\nEnter the value to Enqueue\n");
       scanf("%d",&ITEM);
       QUEUE[R] = ITEM;
     }
-    else
-        {
+    else{
             printf("\nEnter the value to Enqueue\n");
               scanf("%d",&ITEM);
              QUEUE[++R] = ITEM;
         }
 }
 
-void dequeue()
-{
+void dequeue(){
     if (F==-1 && R==-1)
         printf("\nQueue Empty\n");
-    else if (F > R )
-    {
+    else if (F > R ){
       printf("\nQueue Empty, and Resetted\n");
           R = -1;
           F = -1;
     }
-    else  printf("\nQueue dequeued: %d\n",  QUEUE[F++]);
+    else  printf("\nItem Removed: %d\n",  QUEUE[F++]);
 }
-
+void display(){
+      int i;
+      for (i=F; i<=R; i++)
+        printf("\t %d",QUEUE[i]);
+}
 int main()
       {
       int X=0;
       printf("Queue Options\n");
             do {
-                  printf("\n1.Queue Enqueue\n2.Queue Dequeue\n3.Exit for Program\n");
+                  printf("\n1.Enqueue\n2.Dequeue\n3.Display Queue\n4.Exit from Program\n");
                   printf("Select any options ");
                   scanf("%d",&X);
                   switch (X)
@@ -47,7 +46,9 @@ int main()
                                     break;
                         case 2: dequeue();
                                     break;
+                        case 3: display();
+                                    break;
                         default: break;
                   }
-              }while (X==1 || X==2);
+              }while (X==1 || X==2 || X==3);
 }
