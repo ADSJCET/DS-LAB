e10adb3906d11487067e383fb7b020ee78b6bccd
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2022-10-20 14:41:35 +0530

    20-10-2022 (display not working)

---
+++ October/circular-queue.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define MAX 3
+int R = -1, F = -1, ITEM, CQUEUE[MAX];
+void enqueue(){
+
+    if (F==-1 && R==-1){
+      F = R = 0;
+      printf("\nEnter the value to Enqueue\n");
+      scanf("%d",&ITEM);
+      CQUEUE[R] = ITEM;
+    }
+    else if ((R+1) % MAX == F)
+        printf("\nCQueue Full\n");   
+    else{
+         R = (R+1) % MAX;
+         printf("\nEnter the value to Enqueue\n");
+         scanf("%d",&ITEM);
+         CQUEUE[R] = ITEM;
+        }
+}
+
+void dequeue(){
+    if (F==-1 && R==-1)
+        printf("\nCQueue Empty\n");
+    else if (F == R){
+        printf("\nItem Removed: %d\n",  CQUEUE[F]);
+        R = F = -1;
+    }
+    else
+    {
+        printf("\nItem Removed: %d\n",  CQUEUE[F]);
+        F = ( F+1) % MAX;
+    }
+}
+void display(){
+      int i;     
+     for (i=(F<=R?F:R); (F<=R?i<=R:i>F); (F<=R?i++:i--))
+       printf("\t %d",CQUEUE[i]);
+}
+int main()
+      {
+      int X;
+      printf("Circular Queue Options\n");
+            do {
+                  printf("\n1.Enqueue\n2.Dequeue\n3.Display Queue\n4.Exit from Program\n");
+                  printf("Select any options ");
+                  scanf("%d",&X);
+                  switch (X)
+                  {
+                        case 1: enqueue();
+                                    break;
+                        case 2: dequeue();
+                                    break;
+                        case 3: display();
+                                    break;
+                        default: X=0;
+                                    break;
+                  }
+              }while (X!=0);
+}
